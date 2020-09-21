9aa0645485e64b9c242ce32557a9abff5676413c
Ilya Taldykin <rock_cell@mail.ru>
2020-09-21 14:40:57 +0300

    feat: add extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if(options.repeatTimes){
+    let result=[];
+    for(let i=0;i<options.repeatTimes;i++){
+      if(options.addition||options.addition===false||options.addition===null){
+        result.push(str+addition(options));
+      }else{
+        result.push(str);
+      }
+    }
+    if(options.separator){
+      return result.join(options.separator);
+    }else{
+      return result.join('+');
+    }
+  }else {
+    if(options.addition||options.addition===false||options.addition===null){
+      return str+addition(options);
+    }else{
+      return str;
+    }
+  }
 };
+function addition(obj) {
+  if(obj.additionRepeatTimes){
+    let addition=[];
+    for(let i=0;i<obj.additionRepeatTimes;i++){
+      addition.push(String(obj.addition));
+    }
+    if(obj.additionSeparator){
+      return addition.join(obj.additionSeparator);
+    }else{
+      return addition.join('|');
+    }
+  }else{
+    return String(obj.addition);
+  }
+}
+
   
