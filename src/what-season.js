b43d05c8fe195bf660e530f8b2e1032048db8a0e
Ilya Taldykin <rock_cell@mail.ru>
2020-09-22 11:03:57 +0300

    feat: add what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (date) {
+        if (Date.prototype.isPrototypeOf(date)&& !isNaN(date)) {
+
+          let month=date.getMonth();
+          return (month>=0&&month<2||month===11)?'winter':
+              (month>=2&&month<5)?'spring':
+                  (month>=5&&month<8)?'summer':
+                      (month>=8&&month<11)?'autumn':'';
+
+        } else {
+            throw new Error('Invalid or spy Date');
+        }
+    } else {
+        return 'Unable to determine the time of year!';
+    }
 };
